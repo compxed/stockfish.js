6b43901ce39abf1e2e385a18b93e9032668d98f6
Bartosz Paprzycki <poczta@bpaprzycki.pl>
2026-08-08 18:18:14 +0200

    Avoid native macOS flags in WebAssembly builds

--- src/emscripten/wasm-makefile.mk
+++ src/emscripten/wasm-makefile.mk
@@ -7,6 +7,11 @@ arch = wasm
 bits = 64
 SUPPORTED_ARCH=true
 
+# The target is WebAssembly regardless of the build host. In particular, do
+# not let Stockfish's native Darwin settings pass -arch wasm,
+# -mmacosx-version-min, or -mdynamic-no-pic to Emscripten.
+KERNEL := Emscripten
+
 ifeq ($(ASMJS),yes)
 	EM_LDFLAGS  += -s WASM=0
 	popcnt = no
